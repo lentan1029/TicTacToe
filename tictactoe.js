bb1af190bd952c9fe5383c7ba0d67dd2718c46d6
lentan <lentan1029@gmail.com>
2016-10-18 12:26:29 -0700

    set up env for tictactoe

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,64 @@
 const prompt = require('prompt');
+
+const playerNames = [];
+const players = [];
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+  }
+}
+
+class Board {
+  constructor() {
+    this.board = [
+      ['-', '-', '-'],
+      ['-', '-', '-'],
+      ['-', '-', '-'],
+    ];
+  }
+}
+
+class Game {
+  constructor(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.board = new Board();
+    const toStart = Math.floor(Math.random() * 2);
+    if (toStart === 0) {
+      this.startingPlayer = 1;
+    } else {
+      this.startingPlayer = 2;
+    }
+    this.turn = this.startingPlayer;
+  }
+}
+
+prompt.start();
+prompt.get(['Please enter first player\'s name', 'Please enter second player\'s name'], (err, data) => {
+  if (err) {
+    throw new Error(err);
+  } else {
+    playerNames.push(data);
+  }
+});
+
+playerNames.forEach((name) => {
+  players.push(new Player(name));
+});
+
+const startGame = (p1, p2) => {
+  const game = new Game(p1, p2);
+  console.log(game);
+};
+
+prompt.get(['Do you wish to start a game? (y/n)'], (err, data) => {
+  if (err) {
+    throw new Error(err);
+  } else if (data === 'y') {
+    const game = startGame(players[0], players[1]);
+  } else {
+    console.log('Program ending...');
+  }
+});
